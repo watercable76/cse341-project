f36923b94bd023d86fef7a2ab159c6941c3b9268
Nathan Birch <birchn@byui.edu>
2021-03-26 06:15:31 -0600

    te

--- routes/ta02.js
+++ routes/ta02.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.render('pages/ta02', { 
+    res.render('pages', { 
         title: 'Team Activity 02', 
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
